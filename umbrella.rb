6532d6b09313535244f6f17e840ee184f34ba17f
kateleext <83829378+kateleext@users.noreply.github.com>
2025-01-23 16:53:23 +0000

    v2

--- umbrella.rb
+++ umbrella.rb
@@ -7,9 +7,9 @@ require "json"
 require "ascii_charts"
 
 pp "Where are you?"
-#user_location = gets.chomp.gsub(" ","+")
-user_location = "hong kong"
-pp "Checking the weather at #{user_location}..."
+user_location = gets.chomp
+pp "Checking the weather for #{user_location}..."
+user_location = user_location.gsub(" ","+")
 
 #get the geolocation from google maps
 gmaps_key = ENV.fetch("GMAPS_KEY")
@@ -31,7 +31,23 @@ current_summary = pirate_response.fetch("currently").fetch("summary")
 pp "It is currently #{current_summary}, at #{current_temp} degrees."
 
 #next hour precipitation
-next_hour_precip_prob = pirate_response.fetch("hourly").fetch("data").fetch("precipProbability") * 100
-next_hour_precip_type = pirate_response.fetch("hourly").fetch("data").fetch("precipType").downcase
-if next_hour_prob > 0:
+next_hour_precip_prob = pirate_response.fetch("hourly").fetch("data")[0].fetch("precipProbability") * 100
+next_hour_precip_type = pirate_response.fetch("hourly").fetch("data")[0].fetch("precipType").downcase
+if next_hour_precip_prob > 0
   pp "There is a #{next_hour_precip_prob}% chance of #{next_hour_precip_type} in the next hour."
+end
+
+#rain chart
+#based on the docs, i need an array of x-y pairs
+chart_data = []
+hourly_forecast = pirate_response.fetch("hourly").fetch("data")[0,12]
+
+hourly_forecast.each_with_index do |forecast,count|
+  precip = forecast.fetch("precipProbability")
+  hour = count+1
+  data = [hour,precip]
+  chart_data.push(data)
+end
+puts "-----------------"
+puts "Hours from Now vs. Percipitation Probability"
+puts AsciiCharts::Cartesian.new(chart_data, :bar => true).draw
